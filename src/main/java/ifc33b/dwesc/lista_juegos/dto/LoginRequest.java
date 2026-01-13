9529d5aafc76b5b89d7f43bfa1650da0113503e3
Cristian Pérez <cperezjuarez@cifpfbmoll.eu>
2026-01-13 17:42:04 +0100

    Create: dto - LoginRequest.java

---
+++ src/main/java/ifc33b/dwesc/lista_juegos/dto/LoginRequest.java
@@ -0,0 +1,18 @@
+package ifc33b.dwesc.lista_juegos.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+    @NotBlank(message = "Introduce el nombre de usuario")
+    private String username;
+    
+    @NotBlank(message = "Introduce la contraseña")
+    private String password;
+
+    public LoginRequest(String username, String password) {
+        this.setUsername(username);
+        this.setPassword(password);
+    }
+}
