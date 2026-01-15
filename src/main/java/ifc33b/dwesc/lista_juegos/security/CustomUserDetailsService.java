922c425fef924b81f32abd2ef9307724cafae912
Cristian Pérez Juárez <cristianpj0808@gmail.com>
2026-01-15 15:34:48 +0100

    Create: Servicio CustomUserDetailsService.java

---
+++ src/main/java/ifc33b/dwesc/lista_juegos/security/CustomUserDetailsService.java
@@ -0,0 +1,36 @@
+package ifc33b.dwesc.lista_juegos.security;
+
+import java.util.Collections;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import ifc33b.dwesc.lista_juegos.model.User;
+import ifc33b.dwesc.lista_juegos.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    // Crear un UserDetails a partir de un User
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .authorities(Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + user.getRole())))
+                .accountExpired(false)
+                .accountLocked(false)
+                .credentialsExpired(false)
+                .disabled(!user.isEnabled())
+                .build();
+    }
+}
