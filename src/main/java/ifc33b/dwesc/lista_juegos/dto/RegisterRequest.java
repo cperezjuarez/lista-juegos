8616ba0f9d50a8181067a82f843f9c84d54f68a8
Cristian Pérez Juárez <cristianpj0808@gmail.com>
2026-01-15 14:57:42 +0100

    Create: Modelo RegisterRequest

---
+++ src/main/java/ifc33b/dwesc/lista_juegos/dto/RegisterRequest.java
@@ -0,0 +1,21 @@
+package ifc33b.dwesc.lista_juegos.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+    @NotBlank(message = "Se requiere nombre de usuario")
+    @Size(min = 3, max = 50, message = "El nombre de usuario debe de ser entre 3 y 50 caracteres")
+    private String username;
+    
+    @NotBlank(message = "Se requiere contraseña")
+    @Size(min = 6, message = "La contraseña debe de ser mínimo de 6 caracteres")
+    private String password;
+    
+    @NotBlank(message = "Se requiere email")
+    @Email(message = "El mail debe ser válido")
+    private String email;
+}
